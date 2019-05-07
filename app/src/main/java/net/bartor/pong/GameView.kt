defd83a69000a2e24e833d551bc75705337788ec
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-07 22:23:24 +0200

    working bot

--- app/src/main/java/net/bartor/pong/GameView.kt
+++ app/src/main/java/net/bartor/pong/GameView.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import net.bartor.pong.bot.BasicBot
 import net.bartor.pong.elements.Ball
 import net.bartor.pong.elements.Paddle
 import net.bartor.pong.models.MovementLimist
@@ -13,13 +14,14 @@ import net.bartor.pong.models.QuadraticMovement
 class GameView(context: Context, private val mode: GameMode, private val diff: GameDiff) : SurfaceView(context),
     SurfaceHolder.Callback {
     private val thread: GameThread
-    private lateinit var callback : PointCounter
+    private lateinit var callback: PointCounter
 
-    private lateinit var ball : Ball
-    private lateinit var lPaddle : Paddle
-    private lateinit var rPaddle : Paddle
-    private lateinit var lPaddleMovement : QuadraticMovement
-    private lateinit var rPaddleMovement : QuadraticMovement
+    private lateinit var bot: BasicBot
+    private lateinit var ball: Ball
+    private lateinit var lPaddle: Paddle
+    private lateinit var rPaddle: Paddle
+    private lateinit var lPaddleMovement: QuadraticMovement
+    private lateinit var rPaddleMovement: QuadraticMovement
 
     init {
         holder.addCallback(this)
@@ -51,9 +53,13 @@ class GameView(context: Context, private val mode: GameMode, private val diff: G
         if (ball.y <= 0 || ball.y + ball.getSize() >= height) ball.bounce(false)
         lPaddleMovement.update()
         rPaddleMovement.update()
+
+        if (mode == GameMode.BOT) bot.update().let {
+            lPaddleMovement.onInput(it.first, it.second)
+        }
     }
 
-    private fun collision(ball: Ball, paddle: Paddle) : Boolean {
+    private fun collision(ball: Ball, paddle: Paddle): Boolean {
         val ballX = ball.x + ball.xSpeed + ball.getSize()
         val ballY = ball.y + ball.ySpeed + ball.getSize()
         return ballX > paddle.getX() && ballX < paddle.getX() + paddle.getWidth() && ballY > paddle.getY() && ballY < paddle.getY() + paddle.height
@@ -70,17 +76,17 @@ class GameView(context: Context, private val mode: GameMode, private val diff: G
     }
 
     override fun surfaceCreated(p0: SurfaceHolder?) {
-        lPaddle = Paddle(height / 3f, width / 8f, height/2f)
-        rPaddle = Paddle(height / 3f, 7 * width / 8f, height/2f)
+        lPaddle = Paddle(getDiffHeight(), width / 8f, height / 2f)
+        rPaddle = Paddle(getDiffHeight(), 7 * width / 8f, height / 2f)
 
         lPaddle.setLimits(MovementLimist(0f, 0f, 0f, height.toFloat()))
         rPaddle.setLimits(MovementLimist(0f, 0f, 0f, height.toFloat()))
 
-        lPaddleMovement = QuadraticMovement(lPaddle, 100f/height, 0.3f)
-        rPaddleMovement = QuadraticMovement(rPaddle, 100f/height, 0.3f)
+        lPaddleMovement = QuadraticMovement(lPaddle, 100f / height, 0.3f)
+        rPaddleMovement = QuadraticMovement(rPaddle, 100f / height, 0.3f)
         //set last positions to middle
-        lPaddleMovement.onInput(0f, height/2f - lPaddle.height/2f)
-        rPaddleMovement.onInput(0f, height/2f - rPaddle.height/2f)
+        lPaddleMovement.onInput(0f, height / 2f - lPaddle.height / 2f)
+        rPaddleMovement.onInput(0f, height / 2f - rPaddle.height / 2f)
 
         nextRound()
 
@@ -91,16 +97,17 @@ class GameView(context: Context, private val mode: GameMode, private val diff: G
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         for (i in 0 until event!!.pointerCount) {
             if (event.getX(i) < width / 2f) {
-                lPaddleMovement.onInput(event.getX(i), event.getY(i) - lPaddle.height/2f)
+                if (mode != GameMode.BOT) lPaddleMovement.onInput(event.getX(i), event.getY(i) - lPaddle.height / 2f)
             } else {
-                rPaddleMovement.onInput(event.getX(i), event.getY(i) - rPaddle.height/2f)
+                rPaddleMovement.onInput(event.getX(i), event.getY(i) - rPaddle.height / 2f)
             }
         }
         return true
     }
 
     private fun nextRound() {
-        ball = Ball(20f, width/2f, height/2f)
+        ball = Ball(getDiffSpeed(), width / 2f, height / 2f)
+        if (mode == GameMode.BOT) bot = BasicBot(ball, diff)
     }
 
     override fun surfaceDestroyed(p0: SurfaceHolder?) {
@@ -117,6 +124,22 @@ class GameView(context: Context, private val mode: GameMode, private val diff: G
     interface PointCounter {
         fun onPointCount(left: Boolean)
         fun onBounce()
+    }
+
+    private fun getDiffSpeed(): Float {
+        return when (diff) {
+            GameDiff.EASY -> 15f
+            GameDiff.MEDIUM -> 20f
+            GameDiff.HARD -> 25f
+        }
+    }
+
+    private fun getDiffHeight(): Float {
+        return when (diff) {
+            GameDiff.EASY -> height / 2f
+            GameDiff.MEDIUM -> height / 3f
+            GameDiff.HARD -> height / 4f
+        }
     }
 }
 
---
+++ app/src/main/java/net/bartor/pong/bot/BasicBot.kt
@@ -0,0 +1,34 @@
+package net.bartor.pong.bot
+
+import net.bartor.pong.GameDiff
+import net.bartor.pong.elements.Ball
+import java.util.*
+
+class BasicBot(private var ball: Ball, private val diff: GameDiff) {
+    private var moves = LinkedList<Pair<Float, Float>>()
+
+    init {
+        for (i in 0 until getDiffIterationSize()) moves.add(Pair(ball.x, ball.y))
+    }
+
+    fun update() : Pair<Float, Float> {
+        moves.add(Pair(ball.x + getInFrontItersSize()*ball.xSpeed, ball.y + getInFrontItersSize()*ball.ySpeed))
+        return moves.removeFirst()
+    }
+
+    private fun getDiffIterationSize(): Int {
+        return when (diff) {
+            GameDiff.EASY -> 10
+            GameDiff.MEDIUM -> 5
+            GameDiff.HARD -> 1
+        }
+    }
+
+    private fun getInFrontItersSize(): Int {
+        return when (diff) {
+            GameDiff.EASY -> 0
+            GameDiff.MEDIUM -> 2
+            GameDiff.HARD -> 5
+        }
+    }
+}
