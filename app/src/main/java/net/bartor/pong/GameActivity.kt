09dc50468883d2ad57ad74a307bd0eb5b5a9fcfe
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-07 10:34:53 +0200

    added score to sharedPrefs

--- app/src/main/java/net/bartor/pong/GameActivity.kt
+++ app/src/main/java/net/bartor/pong/GameActivity.kt
@@ -1,5 +1,7 @@
 package net.bartor.pong
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -8,19 +10,31 @@ import kotlinx.android.synthetic.main.activity_game.*
 class GameActivity : AppCompatActivity(), GameView.PointCounter {
     private var lPoints = 0
     private var rPoints = 0
+    private lateinit var prefs : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+        prefs = getPreferences(Context.MODE_PRIVATE)
+        lPoints = prefs.getInt("l", 0)
+        rPoints = prefs.getInt("r", 0)
+
         val game = GameView(this)
         game.setOnPointCounter(this)
         gameContainer.addView(game)
+        findViewById<TextView>(R.id.scoreText).text = "$lPoints:$rPoints"
     }
 
     override fun onPointCount(left: Boolean) {
         if (left) rPoints++
         else lPoints++
+
+        with(prefs.edit()) {
+            putInt("l", lPoints)
+            putInt("r", rPoints)
+            apply()
+        }
 
         runOnUiThread {
             findViewById<TextView>(R.id.scoreText).text = "$lPoints:$rPoints"
